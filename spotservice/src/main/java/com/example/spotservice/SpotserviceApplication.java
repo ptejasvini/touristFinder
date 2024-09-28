e7751abafd35b4e717b9cf0a6a8af2438fdb124d
tejasvinip <33099251+ptejasvini@users.noreply.github.com>
2024-09-28 05:28:32 +0000

    Initial commit

---
+++ spotservice/src/main/java/com/example/spotservice/SpotserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotserviceApplication.class, args);
+	}
+
+}
